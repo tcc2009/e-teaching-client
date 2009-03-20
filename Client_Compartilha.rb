f06bd7e00bbb3acad4548b790ab434105b096344
Hebert Santana <hebert.uesc@gmail.com>
2009-03-20 09:18:25 -0300

    Esse aqui é o codigo que vai rodar no cliente! Ele recebe uma msgm e inicia o compartilhamento.

---
+++ Client_Compartilha.rb
@@ -0,0 +1,23 @@
+require 'socket'
+require 'ipaddr'
+
+MULTICAST_ADDR = "225.4.5.6" 
+PORT = 5000
+
+ip =  IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new("0.0.0.0").hton
+
+sock = UDPSocket.new
+sock.setsockopt(Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP, ip)
+sock.bind(Socket::INADDR_ANY, PORT)
+
+loop do
+  msg, info = sock.recvfrom(1024)
+  if msg == 'yes'
+    x = Thread.new {`xtightvncviewer -fullscreen -viewonly 192.168.1.72`}  
+    #puts 'compartilhando'
+  else
+    #Thread.kill x
+    `killall xtightvncviewer`
+    #puts 'nada' 
+  end
+end
